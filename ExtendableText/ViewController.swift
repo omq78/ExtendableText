8a024758a89c6bb6826b0d30fd2751b6807f5c74
omq78 <omar.qab@gmail.com>
2019-07-24 22:48:13 +0300

    Initial Commit

---
+++ ExtendableText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExtendableText
+//
+//  Created by Omar Alqabbani on 7/24/19.
+//  Copyright © 2019 OmarALqabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
